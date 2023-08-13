90ea0bf21ee9f04da0329436174422b8fedee677
Hamidou.Sow <hamidou.sow@hotmail.fr>
2023-08-13 02:17:12 +0200

    comment url in deleteAlert

--- cdaflix-dashboard/src/components/DeleteAlert/DeleteAlert.tsx
+++ cdaflix-dashboard/src/components/DeleteAlert/DeleteAlert.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function DeleteAlert() {
-    const deleteUrl = `http://localhost:8081/api/film/deleteMovie?_id=${film._id}`;
+    //const deleteUrl = `http://localhost:8081/api/film/deleteMovie?_id=${props._id}`;
 
     return (
         
